1c83f37ce0832f88d70f2b9277ff516b683e10f3
Karlan <Karlan@COOLSILENCE>
2023-09-21 11:40:55 +0200

    Add project files.

---
+++ Paddle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Breakout
+{
+    internal class Paddle
+    {
+        int x = 550;
+        int y = 350;
+        int width = 50;
+        int height = 30;
+
+        public Paddle(int x, int y, int width, int height)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+        }
+    }
+}
